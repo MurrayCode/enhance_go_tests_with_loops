2939554f5f89f105eb301703a671bc020f9a8be0
MurrayCode <Murraystewart94@hotmail.co.uk>
2022-10-16 17:21:49 +0100

    refactor:Added name into tests

--- after/add_test.go
+++ after/add_test.go
@@ -4,24 +4,28 @@ import "testing"
 
 func TestAddNums(t *testing.T) {
 	var tests = []struct {
+		name   string
 		inputs []int
 		ans    int
 	}{
 		{
+			"Two positive integers",
 			[]int{2, 2},
 			4,
 		},
 		{
+			"Two negative integers",
 			[]int{-10, -10},
 			-20,
 		},
 		{
+			"One positive, one negative integer",
 			[]int{-1, 3},
 			2,
 		},
 	}
 	for _, tt := range tests {
-		t.Run("returns correct integer", func(t *testing.T) {
+		t.Run(tt.name, func(t *testing.T) {
 			got := AddNums(tt.inputs[0], tt.inputs[1])
 			exp := tt.ans
 			if got != exp {
@@ -34,20 +38,23 @@ func TestAddNums(t *testing.T) {
 
 func TestCombineString(t *testing.T) {
 	var tests = []struct {
+		name   string
 		inputs []string
 		ans    string
 	}{
 		{
+			"test name one",
 			[]string{"test", "ing"},
 			"testing",
 		},
 		{
+			"test name two",
 			[]string{"foo", "bar"},
 			"foobar",
 		},
 	}
 	for _, tt := range tests {
-		t.Run("return correct string", func(t *testing.T) {
+		t.Run(tt.name, func(t *testing.T) {
 			got := CombineString(tt.inputs[0], tt.inputs[1])
 			exp := tt.ans
 			if got != exp {
